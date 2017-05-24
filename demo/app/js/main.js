6da76674b88fb12396ccbf3e960a33aba9c57383
Raphael <raphael.brand@devugees.org>
2017-05-24 10:25:39 +0100

    textbuffer added

--- demo/app/js/main.js
+++ demo/app/js/main.js
@@ -1,7 +1,15 @@
 const log = console.log;
 
 log('Hello Bootstrap');
-require(['animation', 'test-module'], function(console) {
+require(['animation', 'test-module'], function (console) {
   console.animateText('.animation', 150);
+});
+
+require(['textbuffer'], function (TextBuffer) {
+  var t = TextBuffer().obj;
+
+  window.addEventListener('keyup', () => {
+    console.log(t.getBufferedText());
+  });
 });
 
