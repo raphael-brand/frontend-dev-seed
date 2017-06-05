84754e6a1e55e6ec91b95960254db7020635bb36
Raphael Brand <info@raphaelbrand.de>
2017-06-05 20:34:04 +0200

    removing require definition of test-module

--- demo/dist/js/main.js
+++ demo/dist/js/main.js
@@ -1,8 +1,3 @@
-define('test-module', function() {
-  return {
-    test: () => {return 'this is a test'}
-  }
-});
 const log = console.log;
 log('Hello Bootstrap');
 require(['bundle'], function() {
