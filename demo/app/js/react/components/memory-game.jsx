0e0597b90516cd605125d6232cba954a3169c2ae
Raphael Brand <info@raphaelbrand.de>
2017-06-07 01:18:11 +0200

    adding components Card and Sets

--- demo/app/js/react/components/memory-game.jsx
+++ demo/app/js/react/components/memory-game.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
 import {render} from 'react-dom';
 
+class Card extends React.Component {
+  render() {
+    return (
+      <div className="card">{this.props.name}</div>
+    );
+  }
+}
+
+class Sets extends React.Component {
+  render() {
+    var rows = new Array();
+    for(var i=0; i <= 5; i++) {
+      rows.push(<Card name={i} key={i} />);
+    }
+
+    return <div> {rows} </div>
+  }
+}
+
 class MemoryGame extends React.Component {
   render() {
     return (
-      <div class="memory-game">Memory Game</div>
+      <div className="memory-game">Memory Game<Sets /></div>
+      
     );
   }
 }
