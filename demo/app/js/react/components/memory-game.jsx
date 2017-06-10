fb9b6193162f44f6c5f8eb3d52f0e55259035e98
Raphael Brand <info@raphaelbrand.de>
2017-06-10 20:52:30 +0200

    added card flip function

--- demo/app/js/react/components/memory-game.jsx
+++ demo/app/js/react/components/memory-game.jsx
@@ -2,9 +2,29 @@ import React from 'react';
 import {render} from 'react-dom';
 
 class Card extends React.Component {
+  
+  flipped: boolean;
+  constructor(props) {
+    super(props);
+    this.flipped = false;
+    this.flipCard = this.flipCard.bind(this);
+  }
+
+
+  flipCard(event) {
+    this.flipped = !this.flipped;
+    if(!this.flipped)
+      event.target.classList.add('flipped');
+    else
+      event.target.classList.remove('flipped');
+  }
+  
   render() {
     return (
-      <div className="card">{this.props.name}</div>
+      <div
+        className="card"
+        onClick={this.flipCard}
+      >{this.props.name}</div>
     );
   }
 }
