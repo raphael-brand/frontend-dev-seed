ba93da4f62ba292632e02b6c7fed8399007f6d83
Raphael Brand <info@raphaelbrand.de>
2017-06-17 16:42:46 +0200

    adding background image placeholder for a card from json

--- demo/app/js/react/components/memory-game/Card.jsx
+++ demo/app/js/react/components/memory-game/Card.jsx
@@ -5,19 +5,20 @@ class Card extends React.Component {
   
   constructor(props) {
     super(props);
-    this.state = {"cardFlipped": 'card'}
+    this.state = {"cardFlipped": 'card', "backgroundImage":this.props.image}
   }
 
   flipCard() {
     var css = (this.state.cardFlipped == 'card' ? 'card flipped' : 'card' )
     this.setState({"cardFlipped":css});
   }
-  
+
   render() {
     return (
       <div
         className={this.state.cardFlipped}
         onClick={this.flipCard.bind(this)}
+        style={{backgroundImage:'url('+this.props.image+')'}}
       >{this.props.name}</div>
     );
   }
--- demo/app/js/react/components/memory-game/Sets.jsx
+++ demo/app/js/react/components/memory-game/Sets.jsx
@@ -3,12 +3,26 @@ import {render} from 'react-dom';
 import {Card} from './Card';
 
 class Sets extends React.Component {
+  that:any;
+  constructor() {
+    super();
+    this.state = {
+      backgroundImage:''
+    }
+  }
+  componentWillMount() {
+    fetch('js/data.json')
+        .then(response => response.json())
+        .then(result => {
+          console.log(result.url)
+          this.setState({"backgroundImage":result.url})
+        });
+  }
   render() {
     var rows = new Array();
     for(var i=0; i <= 5; i++) {
-      rows.push(<Card name={i} key={i} />);
+      rows.push(<Card name={i} image={this.state.backgroundImage} key={i} />);
     }
-
     return <div> {rows} </div>
   }
 }
