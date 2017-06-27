49ad274aa8fd9c5ad6185ce2b3fe1e1f88b73f2d
Raphael Brand <info@raphaelbrand.de>
2017-06-27 11:06:40 +0200

    moved card creation to card component

--- demo/app/js/memory-game/card.js
+++ demo/app/js/memory-game/card.js
@@ -1,7 +1,19 @@
 define('card', function() {
   return function(id) {
-      var div = document.createElement('div');
-      div.innerHTML = `<div class="card" id="${id}"></div>`;
-      return div.querySelector('.card');
+      var Card = `<div class="cardWrapper">
+      <div class="card" id="${id}"></div>
+      </div>`;
+      var wrapper = document.createElement('div');
+      wrapper.innerHTML = Card;
+      var Card = wrapper.querySelector('.card');
+      Card.style.backgroundImage = 'url(../img/' + id + '.jpeg)';
+      Card.addEventListener('click', (e)=> {
+        var e = e.target;
+        if(!(e.className.indexOf('flipped') > -1))
+          e.classList.add('flipped');
+        else
+          e.classList.remove('flipped');
+      });
+      return wrapper;
     }
 });
--- demo/app/js/memory-game/sets.js
+++ demo/app/js/memory-game/sets.js
@@ -3,24 +3,8 @@ define('sets', ['./card'], function(card) {
         .then(result => {
           for(var item of result) {
             console.log(item.id);
-            var cardWrapper = '<div class="cardWrapper"></div>';
-            var tmp = document.createElement('div');
-            tmp.innerHTML = cardWrapper;
-            document.getElementById('memory-game').appendChild(tmp.querySelector('.cardWrapper'));
-            var Card = card(item.id);
-            Card.style.backgroundImage = 'url(../img/'+item.id + '.jpeg)';
-            Card.addEventListener('click', (e)=> {
-              var e = e.target;
-              if(!(e.className.indexOf('flipped') > -1))
-                e.classList.add('flipped');
-              else
-                e.classList.remove('flipped');
-              
-            });
-            
-            for(let card of document.getElementsByClassName('cardWrapper')) {
-              card.appendChild(Card);
-            }
+            var Card = card(item.id)
+            document.querySelector('#memory-game').appendChild(Card);
           }
       }
   );
